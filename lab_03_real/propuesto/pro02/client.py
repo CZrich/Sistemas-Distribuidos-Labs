40548f77ea13dcfec16aac18e87e43972c558bb0
ianthony4 <achaisa@unsa.edu.pe>
2025-05-06 00:16:29 -0500

    Creacion del cliente

--- lab_03_real/propuesto/pro02/client.py
+++ lab_03_real/propuesto/pro02/client.py
@@ -19,4 +19,72 @@ class Client:
     
     def start(self):
         # Intento de conexión con el servidor
-        try
+        try:
+            self.sock.connect((self.host, self.port))
+        except Exception as e:
+            print(f"No se pudo conectar al servidor: {e}")
+            return
+        
+        # Envio del nombre de usuario como primer mensaje ( NO serializado)
+        self.username = input("Ingrese nombre de usuario: ")
+        self.sock.send(self.username.encode())
+
+        # Intrucciones
+        print("\n¡Bienvenido al chat EPIS!!")
+        print("Lista de comandos disponibles:")
+        print("1. Escribe un mensaje y presiona Enter para enviarlo a todos.")
+        print("2. Escribe '@usuario mensaje' para enviar un mensaje privado.")
+        print("3. Escribe 'WHOISIN' para ver quiénes están conectados.")
+        print("4. Escribe 'LOGOUT' para salir del chat.\n")
+
+        # Hilo para los mensajes de servidor en segundo plano
+
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.daemon = True # El hilo cierra si el programa principal igual
+        receive_thread.start()
+
+        # Bucle para los mensajes desde consola
+
+        while True:
+            msg = input("> ")
+            if msg.upper == "LOGOUT":
+                self.send.message(LOGOUT, "")
+                break # Cortamos el bucle y cerramos el socket
+            elif msg.upper() == "WHOISIN":
+                self.send_message(WHOISIN, "")
+            else:
+                self.send_message(MESSAGE, msg)
+
+        self.sock.close() # Cierre de conexion
+    
+    def send_message(self, msg_type, content):
+        # Diccionario para representar el ChatMessage
+        message = {
+            "type": msg_type,
+            "content": content
+
+        }
+        try:
+            # Serializamos el mensaje y lo enviamos
+            self.sock.send(pickle.dumps(message))
+        except:
+            print("Error al enviar el mensaje")
+            self.sock.close()
+    
+    def receive_messages(self):
+        # Hilo que esta atento al servidor
+        while True:
+            try:
+                data = self.sock.recv(4096)
+                if not data:
+                    break
+                message = pickle.loads(data)
+                print("\n"+message+"\n> ",end="")
+            except:
+                print("\n[Conexion cerrada por el servidor]")
+                break
+if __name__ == "__main__":
+    # Cliente con valores por defecto
+    client = Client()
+    client.start()
+        
