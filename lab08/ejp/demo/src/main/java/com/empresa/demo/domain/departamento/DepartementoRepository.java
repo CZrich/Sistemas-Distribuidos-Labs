6076e509880df79afb6328db00cf2dcc8070c32d
CZrich <czrichardz@gmail.com>
2025-06-16 15:46:49 -0500

    eliminando archivos duplicados

--- lab08/ejp/demo/src/main/java/com/empresa/demo/domain/departamento/DepartementoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.empresa.demo.domain.departamento;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartementoRepository extends JpaRepository<Departamento,Long> {
-}
